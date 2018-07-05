559891c2fae0f1eb98156aa1f8a1ed0198b2ddd3
Pham Van Hai <haivan.qn@gmail.com>
2018-07-05 21:57:47 +0700

    Add Insert and Update api

--- networking/Server.js
+++ networking/Server.js
@@ -8,6 +8,8 @@ import {View, Text,
 } from 'react-native'
 
 const apiGetAllFoods = 'https://next.json-generator.com/api/json/get/4kNy0-PfH'
+const apiInsertNewFood = 'https://next.json-generator.com/api/json/get/4kNy0-PfH'
+const apiUpdateFood = 'https://next.json-generator.com/api/json/get/4kNy0-PfH'
 
 async function getFoodsFromServer() {
     try {
@@ -20,4 +22,40 @@ async function getFoodsFromServer() {
     }
 }
 
+async function insertNewFoodToServer(params) {
+    try{
+        let response = await fetch(apiInsertNewFood, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(params)
+        })
+        let responseJson = await response.json()
+        return responseJson.result
+    } catch (error){
+        console.error(`Error is: ${error}`)
+    }
+}
+
+async function updateFoodToServer(params) {
+    try{
+        let response = await fetch(apiUpdateFood, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(params)
+        })
+        let responseJson = await response.json()
+        return responseJson.result
+    } catch (error){
+        console.error(`Error is: ${error}`)
+    }
+}
+
 export {getFoodsFromServer}
+export {insertNewFoodToServer}
+export {updateFoodToServer}
